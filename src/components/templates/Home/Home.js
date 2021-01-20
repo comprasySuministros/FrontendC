74e4a6001aed4e7eb61e6bb3110e87558c6da241
cesar <csgalindos@hotmail.com>
2021-01-20 08:16:08 -0500

    [updated] call API

--- src/components/templates/Home/Home.js
+++ src/components/templates/Home/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
 import React, { useState } from 'react';
@@ -11,10 +12,9 @@ export default function Home() {
   const history = useHistory();
   const { Header, Content } = Layout;
   const [search, setSearch] = useState('');
-  const API = 'http://localhost:3000/api/articles';
+  const API = 'https://backend-c.chestergalindo.vercel.app/api/articles';
   const initialState = useInitialState(API);
-  const articles = Object.values(initialState);
-  console.log(articles);
+  const articles = initialState.data;
 
   const editArticle = (id) => {
     history.push(`articulos/actualizar/${id}`);
@@ -41,43 +41,44 @@ export default function Home() {
           />
         </section>
         <section className="layout-article_groupcard">
-          {articles.map((article) => (
-            <div className="layout-article_card" key={article.name}>
-              <img src={articles.image} alt={article.name} />
-              <span className="information">
-                <p>
-                  <strong>ID: </strong>
-                  {article.IdPS}
-                </p>
-                <p>
-                  <strong>Nombre: </strong>
-                  {article.name}
-                </p>
-                <p>
-                  <strong>Presentación: </strong>
-                  {article.presentation}
-                </p>
-                <p>
-                  <strong>Precio: </strong>
-                  {article.Price}
-                </p>
-                <p>
-                  <strong>Concepto tecnico: </strong>
-                  {article.concept}
-                </p>
-                <button type="button">
-                  <AiTwotoneDelete />
-                </button>
-                <button type="button">
-                  <AiFillEdit
-                    onClick={() => {
-                      editArticle(article.id);
-                    }}
-                  />
-                </button>
-              </span>
-            </div>
-          ))}
+          {articles
+             && articles.map((article) => (
+              <div className="layout-article_card" key={article.name}>
+                <img src={articles.image} alt={article.name} />
+                <span className="information">
+                  <p>
+                    <strong>ID: </strong>
+                    {article.IdPS}
+                  </p>
+                  <p>
+                    <strong>Nombre: </strong>
+                    {article.name}
+                  </p>
+                  <p>
+                    <strong>Presentación: </strong>
+                    {article.presentation}
+                  </p>
+                  <p>
+                    <strong>Precio: </strong>
+                    {article.Price}
+                  </p>
+                  <p>
+                    <strong>Concepto tecnico: </strong>
+                    {article.concept}
+                  </p>
+                  <button type="button">
+                    <AiTwotoneDelete />
+                  </button>
+                  <button type="button">
+                    <AiFillEdit
+                      onClick={() => {
+                        editArticle(article.id);
+                      }}
+                    />
+                  </button>
+                </span>
+              </div>
+            ))}
         </section>
       </Content>
     </Layout>
