919676150ee4cdc12734ef4ae163ba3089443ae0
cesar <csgalindos@hotmail.com>
2021-01-24 22:04:25 -0500

    [updated] call information in create page

--- src/components/templates/CreateProducts/CreateProducts.js
+++ src/components/templates/CreateProducts/CreateProducts.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 // import { toast } from 'react-toastify';
 import { useParams } from 'react-router-dom';
 import './createProducts.scss';
-import { useFormik } from 'formik';
 import useInitialState from '../../../utils/hooks/initialState';
 
 export default function CreateProducts() {
@@ -12,78 +11,11 @@ export default function CreateProducts() {
   const API = `https://backend-c.chestergalindo.vercel.app/api/articles/${id}`;
   const initialState = useInitialState(API);
   const article = initialState.data;
-  // const [formArticle, setFormArticle] = useState();
-  const formik = useFormik({
-    initialValues: {
-      IdPS: '',
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
 
-  // const inputs = [
-  //   {
-  //     name: 'IDPS',
-  //     placeholder: 'ID de Artículo',
-  //     type: 'number',
-  //   },
-  //   {
-  //     name: 'Name',
-  //     placeholder: 'Nombre de Artículo',
-  //     type: 'text',
-  //   },
-  //   {
-  //     name: 'Presentation',
-  //     placeholder: 'Unidad de medida',
-  //     type: 'text',
-  //   },
-  //   {
-  //     name: 'Price',
-  //     placeholder: 'Precio',
-  //     type: 'number',
-  //   },
-  //   {
-  //     name: 'concept',
-  //     placeholder: 'Concepto tecnico',
-  //     type: 'text',
-  //   },
-  //   {
-  //     name: 'image',
-  //     placeholder: 'imagen',
-  //     type: 'file',
-  //   },
-  // ];
-
-  const getArticleByID = () => {
-    // setValue('Name', article && article.name);
-    // setValue('Presentation', article && article.presentation);
-    // setValue('Price', article && article.Price);
-    // setValue('concept', article && article.concept);
-    // setValue('image', article && article.image);
+  const handleChange = () => {
+    // const { name, value } = e.target;
+    // setFormArticle({ ...formArticle, [name]: value });
   };
-
-  useEffect(() => {
-    console.log(article && article.IdPS);
-    if (id) {
-      getArticleByID(id);
-    }
-  }, [id]);
-
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setFormArticle({ ...formArticle, [name]: value });
-  // };
-
-  // const onSubmit = () => {
-  // if (window.location.pathname.includes('articulos/crear')) {
-  //   addArticle(formArticle);
-  //   toast('Nuevo articulo creado', { type: 'success', position: toast.POSITION.TOP_CENTER });
-  //   reset();
-  // } else {
-  //   updatedArticle();
-  //   toast('articulo Actualizado', { type: 'success', position: toast.POSITION.TOP_CENTER });
-  // };
 
   return (
     <Layout>
@@ -93,18 +25,52 @@ export default function CreateProducts() {
           Formulario Creación de Referencias
         </h2>
         <a href="/"> Home </a>
-        <form onSubmit={formik.handleSubmit}>
-          {/* {inputs.map((input) => ( */}
+        <form>
           <input
             className="layout-article_search-icon input_form"
-            key="IDPS"
+            key="IdPS"
             name="IdPS"
-            onChange={formik.handleChange}
             placeholder="ID de Artículo"
             type="number"
-            value={formik.values.IdPS}
+            value={article && article.IdPS}
+            onChange={handleChange}
           />
-          {/* // ))} */}
+          <input
+            className="layout-article_search-icon input_form"
+            key="name"
+            name="name"
+            placeholder="Nombre de Artículo"
+            type="text"
+            value={article && article.name}
+            onChange={handleChange}
+          />
+          <input
+            className="layout-article_search-icon input_form"
+            key="presentation"
+            name="presentation"
+            placeholder="Unidad de medida"
+            type="text"
+            value={article && article.presentation}
+            onChange={handleChange}
+          />
+          <input
+            className="layout-article_search-icon input_form"
+            key="price"
+            name="price"
+            placeholder="Precio"
+            type="number"
+            value={article && article.Price}
+            onChange={handleChange}
+          />
+          <input
+            className="layout-article_search-icon input_form"
+            key="concept"
+            name="concept"
+            placeholder="concepto tecnico"
+            type="text"
+            value={article && article.concept}
+            onChange={handleChange}
+          />
           <div className="layout-article-button">
             <button className="layout-article-button" type="submit">
               {window.location.pathname.includes('articulos/crear')
@@ -117,6 +83,18 @@ export default function CreateProducts() {
     </Layout>
   );
 }
+
+// const onSubmit = () => {
+// if (window.location.pathname.includes('articulos/crear')) {
+//   addArticle(formArticle);
+//   toast('Nuevo articulo creado', { type: 'success', position: toast.POSITION.TOP_CENTER });
+//   reset();
+// } else {
+//   updatedArticle();
+//   toast('articulo Actualizado', { type: 'success', position: toast.POSITION.TOP_CENTER });
+// };
+
+// const [formArticle, setFormArticle] = useState();
 
 // const addArticle = async (linkObject) => {
 //   await db.collection('articles').doc().set(linkObject);
